659f6b3b948f62043fa6d4269d8f4e88d50e2b8d
pablituuu <keane.pa.si.in@gmail.com>
2026-02-09 04:26:28 -0500

    fix: track page

--- next.config.ts
+++ next.config.ts
@@ -48,7 +48,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
+            value: 'credentialless',
           },
         ],
       },
